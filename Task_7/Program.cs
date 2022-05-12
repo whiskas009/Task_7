1f0d06da82792675c3f221bb78a5df03cdefd47f
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-12 20:52:47 +0300

    #1

---
+++ Task_7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во людей в очереди");
+            int peopleInQueue = int.Parse(Console.ReadLine());
+
+            int minuteInHoures = 60;
+            int waitingTime = 10;
+            int timeHour = (peopleInQueue * waitingTime) / minuteInHoures;
+            int timeMinute = (peopleInQueue * waitingTime) % minuteInHoures;
+
+            Console.WriteLine($"Вы должны отстоять в очереди {timeHour} часа и {timeMinute} минут.");
+        }
+    }
+}
